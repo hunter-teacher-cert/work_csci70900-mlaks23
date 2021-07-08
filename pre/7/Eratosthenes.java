1c762a67b0b9918da2b3362d76ed202371b5f9ef
mlaks23 <mlaks@schools.nyc.gov>
2021-07-07 22:51:04 -0400

    Work in Progress. Prime numbers up to 100 not up to n.

--- pre/7/Eratosthenes.java
+++ pre/7/Eratosthenes.java
@@ -5,18 +5,39 @@ import java.util.Scanner;
 public class Eratosthenes {
 
   public static boolean sieve(int[] a, int n) {
-    //int n = 0;
-    for (int i = 1; i < n; i++) {
-      if (i == 1) {
-        System.out.print(i + "NP, ");    //1 is not prime
-      } else if (i % 2 != 0) {          //Working on a way check divisibility by other numbers
-        System.out.print(i + " P,");
-      } else if (i % 2 == 0) {
-        System.out.print(i + " NP,");
+
+      System.out.println("These are the prime numbers up to 100. ");
+      for (int i = 1; i < 100; i++) {
+          if (i == 1) {
+              System.out.print("");    //1 is not prime
+          } else if ((i == 2)||(i == 3) || (i == 5) || (i == 7)) {
+              System.out.print(i + ", ");  //2 is primee- How do I tell the computer a number with only 2 factors is prime?
+          } else if ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0) || (i % 7 == 0)) {
+              System.out.print("");
+          } else if ((i % 2 != 0) || (i % 3 != 0) || (i % 5 != 0) || (i % 7 != 0)) {
+              System.out.print(i + ", ");
+        }
       }
-    }
-    return true;
+      System.out.println("\n");
+
+/*
+// Trying to figure out how to check divisibility for all integers in the array by all other itegers in the arrays
+// Gives error: java.lang.ArithmeticException: / by zero
+
+      System.out.println("These are the prime numbers up to " + n + ".");
+
+      for (int i = 2; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+          if (s[j] % s[i] != 0) {
+            System.out.print(i + "P ");
+          }
+        }
+      }
+*/
+
+   return true;
   }
+
 
   public static void printArray(int[] a, int n) {
     int [] s = new int [n];
@@ -32,8 +53,8 @@ public class Eratosthenes {
   }
 
 
-
 public static void main(String[] args) {
+  System.out.println("A prime number is a number that is only divisible by 1 and itself. This program will list primes up to the number you choose. ");
   Scanner in = new Scanner(System.in);
   System.out.print("Choose a number n: ");
   int n = in.nextInt();
